e38131e684fb9bae2f064e2a60628c25d45b82d8
IMProgramer <yash.only14u@gmail.com>
2013-01-13 00:50:25 +0530

    Images

---
+++ Chatter/src/Images/Images.java
@@ -0,0 +1,9 @@
+package Images;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class Images {
+	public static Icon facebook = new ImageIcon("facebook_image.png");
+	public static Icon google = new ImageIcon("google_image.png");
+}
