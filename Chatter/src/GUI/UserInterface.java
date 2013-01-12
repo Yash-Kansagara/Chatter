21859ff1686347d4dcca9af143ee0b3c2718744e
IMProgramer <yash.only14u@gmail.com>
2013-01-13 00:45:07 +0530

    User Interface test

--- Chatter/src/GUI/UserInterface.java
+++ Chatter/src/GUI/UserInterface.java
@@ -1,34 +1,32 @@
 package GUI;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.awt.FlowLayout;
+import java.awt.Image;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class UserInterface extends JFrame{
+public class UserInterface extends JFrame {
 
-	JButton facebook_button,google_button;
+	JButton facebook_button, google_button;
+
 	public UserInterface() {
 		super("Chatter");
-		
-		facebook_button = new JButton();
-		google_button = new JButton();
-		try {
-			facebook_button.setIcon(new ImageIcon(new URL("https://www.google.com/images/srpr/logo3w.png")));
-			google_button.setIcon(new ImageIcon(new URL("https://fbstatic-a.akamaihd.net/rsrc.php/v2/yS/x/B1l0pVYCp2E.png")));
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		facebook_button = new JButton("Facebook");
+		google_button = new JButton("Google"); 
+		Icon im = new ImageIcon("Images/facebook_image.png");
+		facebook_button.setIcon(im);
+		google_button.setIcon(new ImageIcon("Images/google_image.png"));
+
 		add(facebook_button);
 		add(google_button);
-		setSize(300,600);
+		setSize(300, 600);
 		setVisible(true);
+		System.out.println(getLayout().toString());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
-	
-	
-	
+
 }
